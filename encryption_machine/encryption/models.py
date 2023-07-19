335dfc5476f2655eeb5df3f6b3e5137dcc13ad20
Dmitriy8854 <dmit.morozoff2009@yandex.ru>
2023-07-19 22:40:19 +0300

    Services and validations

--- encryption_machine/encryption/models.py
+++ encryption_machine/encryption/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 from users.models import User
-from . import services
-from .validations import (validate_key_caesar, validate_text_caesar,
-                          validate_text_morse, validat_qr_vig_aes_text,
-                          validat_vig_aes_text_decrypt, validat_key_aes,
-                          validate_text_morse_decrypt,
-                          validat_key_vigenere)
 
 
 class Encryption(models.Model):
