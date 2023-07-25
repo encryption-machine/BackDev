ec940066f1135fd43bfb627106c00b8dcd7e04e1
Alexey Kachkaev <akachkaev@gmail.com>
2023-07-26 00:15:58 +0500

    Change vigenere validator to RU only 3

--- encryption_machine/encryption/validators.py
+++ encryption_machine/encryption/validators.py
@@ -25,15 +25,10 @@ list_value_morse = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 
                     '>', '#', '%', '*'
                     ]
 
-list_key_vigenere = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к',
-                     'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
-                     'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'
-                     ]
-
-list_value_vigenere = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к',
-                       'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
-                       'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' '
-                       ]
+list_vigenere = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к',
+                 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
+                 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' '
+                 ]
 
 
 def validate_caesar(text, key, is_encryption):
@@ -88,10 +83,10 @@ def validate_vigenere(text, key, is_encryption):
     if len(key) > MAX_KEY_LEN:
         raise ValidationError('Слишком длинный ключ')
     for char in text:
-        if char.lower() not in list_value_vigenere:
+        if char.lower() not in list_vigenere:
             raise ValidationError(f'Вы ввели недопустимый символ {char}')
     for char in key:
-        if char.lower() not in list_key_vigenere:
+        if char.lower() not in list_vigenere:
             raise ValidationError(
                 f'Недопустимый символ {char}. Ключ должен быть словом')
     if key == '':
