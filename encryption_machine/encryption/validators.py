598711653f8fc59dda6bb44e501d58aeefc06730
Alexey Kachkaev <akachkaev@gmail.com>
2023-07-26 00:00:39 +0500

    Change vigenere validator to RU only 2

--- encryption_machine/encryption/validators.py
+++ encryption_machine/encryption/validators.py
@@ -27,8 +27,13 @@ list_value_morse = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 
 
 list_key_vigenere = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к',
                      'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
-                     'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' '
+                     'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'
                      ]
+
+list_value_vigenere = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к',
+                       'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
+                       'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' '
+                       ]
 
 
 def validate_caesar(text, key, is_encryption):
@@ -82,10 +87,13 @@ def validate_vigenere(text, key, is_encryption):
         raise ValidationError('Необходимо ввести ключ')
     if len(key) > MAX_KEY_LEN:
         raise ValidationError('Слишком длинный ключ')
+    for char in text:
+        if char.lower() not in list_value_vigenere:
+            raise ValidationError(f'Вы ввели недопустимый символ {char}')
     for char in key:
-        if char not in list_key_vigenere:
+        if char.lower() not in list_key_vigenere:
             raise ValidationError(
-                f'Недопустимый символ {char}. Ключ должен быть буквой')
+                f'Недопустимый символ {char}. Ключ должен быть словом')
     if key == '':
         raise ValidationError('Вы не ввели ни одного символа.')
 
