8f0b4481d07dec1a0e26ff442da01ef668025bf7
Dmitry Morozov <109239109+Dmitriy8854@users.noreply.github.com>
2023-07-26 12:52:15 +0300

    Update settings.py

--- encryption_machine/encryption_machine/settings.py
+++ encryption_machine/encryption_machine/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY - 64@wl5gnrb!zhr88+%84#&s$3c0=q=8fiqtn*bc3unazh49oly
+SECRET_KEY = 'django-insecure-v5wnldpxhh-r1hkka$t7l2gg+ktft)w)l==c7_vxi+5(fd+xw)'
 # SECRET_KEY = os.getenv('SECRET_KEY')
 # SECRET_KEY = os.getenv("DJANGO_KEY", get_random_secret_key())
 
