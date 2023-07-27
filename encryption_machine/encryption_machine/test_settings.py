a667e25a5cb803733ce7da81bf4ed2957dabb230
Dmitry Morozov <109239109+Dmitriy8854@users.noreply.github.com>
2023-07-27 23:23:25 +0300

    Update test_settings.py

--- encryption_machine/encryption_machine/test_settings.py
+++ encryption_machine/encryption_machine/test_settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from encryption_machine.settings import *
 
+SECRET_KEY='dqsiqisw8zq8j=@ft0=+4kmbc!h@4n_q80+0s8qpp129m'
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATABASES = {
     "default": {
