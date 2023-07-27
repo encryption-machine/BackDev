989cd1731b4cc5cb8ac8aba6aa2c6c6a84c60898
Dmitry Morozov <109239109+Dmitriy8854@users.noreply.github.com>
2023-07-27 18:07:13 +0300

    Update settings.py

--- encryption_machine/encryption_machine/settings.py
+++ encryption_machine/encryption_machine/settings.py
@@ -96,10 +96,10 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('POSTGRES_USER'),
+        'USER': os.getenv('POSTGRES_USER', 'user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '5432')
     }
 }
 
